9efc215217a28388dc5bb77d27c80d51826f8bf3
RabuMia <Excelchen@hotmail.de>
2021-07-29 08:01:02 +0200

    Lösung

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,11 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+let ersteZahl, zweiteZahl, ergebnis;
+
+
+ersteZahl = parseInt(prompt("Bitte geben Sie eine Zahl ein: "));
+zweiteZahl = parseInt(prompt("Bitte geben sie eine zweite Zahl ein:"));
+ergebnis = ersteZahl + zweiteZahl;
+console.log(ersteZahl + " + " + zweiteZahl);
+console.log("Die Summe der Zahlen ist: " + ergebnis);
